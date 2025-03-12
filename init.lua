cd8d21529a2a77c740f3fc9b23bb8b0c27752adf
Orkan Alat <orkan.alat@googlemail.com>
2025-03-13 00:05:06 +0100

    feat: try move keybindings

--- init.lua
+++ init.lua
@@ -883,5 +883,12 @@ require('lazy').setup({
   },
 })
 
+-- [[ CUSTOM STUFF ]]
+
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==') -- move line up(n)
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==') -- move line down(n)
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv") -- move line up(v)
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv") -- move line down(v)
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
