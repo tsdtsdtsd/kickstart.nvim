916000ef4acba4acb2b32b3efa48b7241990df9c
Orkan Alat <orkan.alat@googlemail.com>
2025-03-17 22:46:58 +0100

    feat: some gpls settings

--- lua/kickstart/core/plugins/nvim-lspconfig.lua
+++ lua/kickstart/core/plugins/nvim-lspconfig.lua
@@ -196,7 +196,13 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
-      gopls = {},
+      gopls = {
+        completeUnimported = true,
+        usePlaceholders = true,
+        analyses = {
+          shadow = true,
+        },
+      },
       -- pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
